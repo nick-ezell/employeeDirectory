33e75fba78202c157b3b0ec2e9aff0450ec32218
Nick Ezell <nickolasezell@gmail.com>
2020-05-26 19:17:29 -0500

    Heroku

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ const PORT = process.env.PORT || 3000;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
